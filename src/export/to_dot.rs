ddc926242effb0d285439082526a0872320466ef
Michiel Kalkman <michiel@nosuchtype.com>
2025-01-20 23:20:13 +1100

    chore: remove unused code, fix dot test

--- src/export/to_dot.rs
+++ src/export/to_dot.rs
@@ -114,32 +114,6 @@ mod tests {
                         }
                     ],
                     "edges": [
-                    ],
-                    "nodeconfigs": [
-                        {
-                            "key": "layer1",
-                            "fillcolor": "white",
-                            "fontcolor": "blue",
-                            "style": "filled",
-                            "shape": "diamond"
-                        },
-                        {
-                            "key": "layer2",
-                            "fillcolor": "pink",
-                            "fontcolor": "white",
-                            "style": "filled",
-                            "shape": "circle"
-                        }
-                    ],
-                    "layerconfigs": [
-                        {
-                            "key": "layer1",
-                            "label": "Layer 1"
-                        },
-                        {
-                            "key": "layer2",
-                            "label": "Layer 2"
-                        }
                     ]
                 }),
             )
@@ -147,7 +121,7 @@ mod tests {
 
         assert_eq!(
             res,
-            "\n\ndigraph G {\n\n    layout=\"neato\";\n    rankdir=\"TB\";\n    splines=true;\n    overlap=false;\n    nodesep=\"0.2\";\n    ranksep=\"0.4\";\n    labelloc=\"t\";\n    fontname=\"Lato\";\n    node [ shape=\"plaintext\" style=\"filled, rounded\" fontname=\"Lato\" margin=0.2 ]\n    edge [ fontname=\"Lato\" color=\"#2B303A\" ]\n\n  // Apply styling to each layer\n      {\n        node[ fillcolor=\"\" fontcolor=\"\" style=\"\" shape=\"\" ];\n      }\n      {\n        node[ fillcolor=\"\" fontcolor=\"\" style=\"\" shape=\"\" ];\n      }\n\n  // Place elements in zones\n\n      id1[label=\"Node 1\"];\n      id2[label=\"Node 2\"];\n      id3[label=\"Node 3\"];\n      id4[label=\"Node 4\"];\n\n}\n    "
-            );
+            "\n\ndigraph G {\n    rankdir=\"TB\";\n    splines=true;\n    overlap=false;\n    nodesep=\"0.3\";\n    ranksep=\"1.2\";\n    labelloc=\"t\";\n    fontname=\"Lato\";\n    node [ shape=\"plaintext\" style=\"filled, rounded\" fontname=\"Lato\" margin=0.2 ]\n    edge [ fontname=\"Lato\" color=\"#2B303A\" ]\n\n\n}\n    "
+        );
     }
 }
