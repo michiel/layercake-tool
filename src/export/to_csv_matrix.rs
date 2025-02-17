71e8ee2a354a91baffc70cfaaa806394d2ed1223
Michiel Kalkman <michiel@nosuchtype.com>
2025-02-18 09:04:02 +1100

    feat: don't quote in csv-matrix export

--- src/export/to_csv_matrix.rs
+++ src/export/to_csv_matrix.rs
@@ -1,14 +1,13 @@
 use crate::graph::Graph;
 use crate::plan::RenderConfig;
 use csv::Writer;
+use csv::WriterBuilder;
 use serde_json::json;
 use serde_json::Value as JsonValue;
 use std::collections::HashMap;
 use std::error::Error;
 
 pub fn render(graph: Graph, _render_config: RenderConfig) -> Result<String, Box<dyn Error>> {
-    let mut wtr = Writer::from_writer(vec![]);
-
     fn create_dynamic_2d_array<T: Clone>(rows: usize, cols: usize, default: T) -> Vec<Vec<T>> {
         let mut matrix = Vec::with_capacity(rows);
         for _ in 0..rows {
@@ -40,9 +39,9 @@ pub fn render(graph: Graph, _render_config: RenderConfig) -> Result<String, Box<
     for node in nodes.clone() {
         let i = positions[&node.id];
         // row
-        matrix[i + offset][0] = json!(node.label);
+        matrix[i + offset][0 + 1] = json!(node.label);
         // column
-        matrix[0][i + offset] = json!(node.label);
+        matrix[0 + 1][i + offset] = json!(node.label);
     }
 
     for edge in edges.clone() {
@@ -50,6 +49,10 @@ pub fn render(graph: Graph, _render_config: RenderConfig) -> Result<String, Box<
         let j = positions[&edge.target];
         matrix[i + offset][j + offset] = json!(edge.weight);
     }
+
+    let mut wtr = WriterBuilder::new()
+        .quote_style(csv::QuoteStyle::Never)
+        .from_writer(vec![]);
 
     for row in matrix {
         wtr.write_record(
