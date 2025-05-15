784cc78ae691db359a11e589e8121ccad3e03dd0
Michiel Kalkman <michiel@nosuchtype.com>
2025-05-16 00:31:18 +1000

    feat: minimum node size for dot / LR diagrams

--- src/export/to_dot_hierarchy.rs
+++ src/export/to_dot_hierarchy.rs
@@ -3,23 +3,7 @@ use crate::plan::RenderConfig;
 use std::error::Error;
 
 pub fn render(graph: Graph, render_config: RenderConfig) -> Result<String, Box<dyn Error>> {
-    use serde_json::json;
-
-    let handlebars = crate::common::get_handlebars();
-
-    let res = handlebars.render_template(
-        &get_template(),
-        &json!({
-            "config": render_config,
-            "hierarchy_nodes": graph.get_hierarchy_nodes(),
-            "hierarchy_edges": graph.get_hierarchy_edges(),
-            // "hierarchy_tree": tree,
-            // "flow_nodes": graph.get_non_partition_nodes(),
-            // "flow_edges": graph.get_non_partition_edges(),
-            "layers": graph.get_layer_map(),
-        }),
-    )?;
-    Ok(res)
+    super::renderer::render_template(graph, render_config, &get_template())
 }
 
 pub fn get_template() -> String {
