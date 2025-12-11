367e796a16b146986a7ebecb6ec999ffb6cea8c2
Michiel Kalkman <michiel@nosuchtype.com>
2025-12-11 19:13:33 +1100

    fix: query graphDataList with correct arguments for projections

--- frontend/src/pages/workbench/ProjectionsPage.tsx
+++ frontend/src/pages/workbench/ProjectionsPage.tsx
@@ -28,7 +28,7 @@ const LIST_PROJECTIONS = gql`
 
 const LIST_GRAPHS = gql`
   query ListGraphsForProjections($projectId: Int!) {
-    graphDataList(projectId: $projectId, sourceTypes: ["computed", "manual", "dataset"]) {
+    graphDataList(projectId: $projectId) {
       id
       name
     }
